bef7f81a03c39189636157aa1892bdc6d60fb02f
KlintonICT <chhunklinton91@gmail.com>
2024-05-17 14:31:36 +0700

    feat: create bubble message dispalying

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -6,10 +8,12 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
-          .orderBy('createdAt', descending: false)
+          .orderBy('createdAt', descending: true)
           .snapshots(),
       builder: (ctx, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -31,11 +35,33 @@ class ChatMessages extends StatelessWidget {
         final loadedMessages = snapshot.data!.docs;
 
         return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 40, left: 13, right: 13),
+          reverse: true,
           itemCount: loadedMessages.length,
           itemBuilder: (ctx, index) {
-            return Text(
-              loadedMessages[index].data()['text'],
-            );
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId =
+                nextChatMessage != null ? nextChatMessage['userId'] : null;
+            final nextUserIsSame = currentMessageUserId == nextMessageUserId;
+
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            } else {
+              return MessageBubble.first(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+                userImage: chatMessage['userImage'],
+                username: chatMessage['username'],
+              );
+            }
           },
         );
       },
--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -48,29 +48,26 @@ class _NewMessageState extends State<NewMessage> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 20),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _messageController,
-                textCapitalization: TextCapitalization.sentences,
-                autocorrect: true,
-                enableSuggestions: true,
-                decoration: const InputDecoration(
-                  labelText: 'Send a message...',
-                  border: OutlineInputBorder(),
-                ),
+      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 34),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: const InputDecoration(
+                labelText: 'Send a message...',
+                border: OutlineInputBorder(),
               ),
             ),
-            IconButton(
-              onPressed: _submitMessage,
-              icon: const Icon(Icons.send),
-            )
-          ],
-        ),
+          ),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+          )
+        ],
       ),
     );
   }
